3e96b71f5554add52f31dd9170b9aeaf685f6884
Alex <alexanderpanchev98@gmail.com>
2026-02-20 12:42:24 +0200

    chore: add admin configuration for orders and desserts

--- desserts/admin.py
+++ desserts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from desserts.models import Category, Ingredient, Dessert
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_allergen")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+@admin.register(Dessert)
+class DessertAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category", "is_available")
+    list_filter = ("category", "is_available")
+    search_fields = ("name", "category__name")
+    ordering = ("name",)
+
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+
+
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'customer_phone', 'customer_email', 'created_at', 'status', "total_price_display",)
+    search_fields = ('customer_name', 'customer_email')
+    list_filter = ('status',)
+
+    def total_price_display(self, obj):
+        return obj.total_price()
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'dessert', 'quantity','unit_price')
+    search_fields = ('order__customer_name',)
