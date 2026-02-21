c69e85d48a700d7fa395d74770940bbcdb60649b
Alex <alexanderpanchev98@gmail.com>
2026-02-21 11:24:06 +0200

    chore: Created home page and fixed some bugs

--- Django_Basics_Final_Project/urls.py
+++ Django_Basics_Final_Project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from core.views import home
+
 urlpatterns = [
-    # path("", home, name="home"),
+    path("", home, name="home"),
     path('admin/', admin.site.urls),
     path("desserts/", include("desserts.urls")),
     path("orders/", include("orders.urls")),
