bccc658f6a725737b3a196e4ca4be738adf732e8
Alex <alexanderpanchev98@gmail.com>
2026-02-21 14:17:44 +0200

    chore: Re-worked the home page, added custom css look

--- orders/forms.py
+++ orders/forms.py
@@ -27,6 +27,10 @@ class OrderForm(forms.ModelForm):
         }
     }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["status"].disabled = True
+
 class OrderItemForm(forms.ModelForm):
     class Meta:
         model = OrderItem
